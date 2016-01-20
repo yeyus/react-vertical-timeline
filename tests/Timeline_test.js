610a3616ebad310482aee19c91e28f64d0bc365c
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-01-19 23:53:32 -0800

    moar tests

--- tests/Timeline_test.js
+++ tests/Timeline_test.js
@@ -1,7 +1,10 @@
+/*global sinon:true*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  renderIntoDocument
+  renderIntoDocument,
+  findRenderedDOMComponentWithClass,
+  Simulate
 } from 'react-addons-test-utils';
 import Timeline from '../src/Timeline.jsx';
 
@@ -37,5 +40,17 @@ describe('Timeline', function() {
     expect(timelineNode.className).to.equal('timeline-block');
     expect(timelineNode.children[0].className).to.equal('timeline-line');
     expect(timelineNode.children[1].className).to.equal('timeline-progress');
+  });
+
+  it('should call onSelect when the timeline is clicked after progress', function() {
+    onSelectMock.once();
+    Simulate.click(findRenderedDOMComponentWithClass(timeline, 'timeline-line'));
+    onSelectMock.verify();
+  });
+
+  it('should call onSelect when the timeline is clicked in elapsed progress', function() {
+    onSelectMock.once();
+    Simulate.click(findRenderedDOMComponentWithClass(timeline, 'timeline-progress'));
+    onSelectMock.verify();
   });
 });
