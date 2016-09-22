91825a9b9b6b4ab2d22a9e4dca4d90c81914fa74
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-09-22 15:13:37 -0700

    fix import

--- dist/react-vertical-timeline.js
+++ dist/react-vertical-timeline.js
@@ -64,11 +64,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var _Bookmark2 = _interopRequireDefault(_Bookmark);
 	
+	var _Marker = __webpack_require__(4);
+	
+	var _Marker2 = _interopRequireDefault(_Marker);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	module.exports = {
 	  'Timeline': _Timeline2.default,
-	  'Bookmark': _Bookmark2.default
+	  'Bookmark': _Bookmark2.default,
+	  'Marker': _Marker2.default
 	};
 
 /***/ },
@@ -283,6 +288,71 @@ return /******/ (function(modules) { // webpackBootstrap
 	Bookmark.propTypes = {
 	  children: _react2.default.PropTypes.node,
 	  onSelect: _react2.default.PropTypes.func,
+	  progress: _react2.default.PropTypes.number.isRequired
+	};
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Marker = function (_React$Component) {
+	  _inherits(Marker, _React$Component);
+	
+	  function Marker() {
+	    _classCallCheck(this, Marker);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Marker).apply(this, arguments));
+	  }
+	
+	  _createClass(Marker, [{
+	    key: 'render',
+	    value: function render() {
+	      var style = {
+	        top: this.context.height * (this.props.progress / 100) + 'px'
+	      };
+	
+	      return _react2.default.createElement(
+	        'div',
+	        { className: 'timeline-marker', style: style },
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'text' },
+	          this.props.progress
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return Marker;
+	}(_react2.default.Component);
+	
+	exports.default = Marker;
+	
+	Marker.contextTypes = {
+	  height: _react2.default.PropTypes.number.isRequired
+	};
+	
+	Marker.propTypes = {
 	  progress: _react2.default.PropTypes.number.isRequired
 	};
 
