95c9e820845410afb72575d377890f2e9d71ab38
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-09-22 11:59:56 -0700

    markers support

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 import Timeline from './Timeline.jsx';
 import Bookmark from './Bookmark.jsx';
+import Marker from './Marker.jsx';
 
 module.exports = {
   'Timeline': Timeline,
-  'Bookmark': Bookmark
+  'Bookmark': Bookmark,
+  'Marker': Marker
 };
